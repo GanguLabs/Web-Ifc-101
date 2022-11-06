55263c5203a92325b4664c1ad05e5a1a0d1f2ed5
Gangula2 <gangulaanwesh@gmail.com>
2022-11-06 19:11:42 +0530

    get list of IFCSPACE's from ifc file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { IfcAPI } from "web-ifc/web-ifc-api";
-const ifcFileLocation = "rac_basic_sample_project.ifc"; // dont forget to modify for your ifc filename
+import { IfcAPI, IFCSPACE } from "web-ifc/web-ifc-api";
+const ifcFileLocation = "IFC-Example.ifc"; // example file here: https://github.com/IFCjs/test-ifc-files/blob/main/Others/haus.ifc
 let modelID = 0;
 const ifcapi = new IfcAPI();
 //ifcapi.SetWasmPath("./wasm/"); only if the wasm file are note at the same level as app.js
@@ -27,6 +27,31 @@ ifcapi.Init().then(() => {
         modelID = ifcapi.OpenModel(ifcData);
         let isModelOpened = ifcapi.IsModelOpen(modelID);
         console.log({ isModelOpened });
+        let spaces = getAllSpaces(modelID);
+        console.log({ spaces });
         ifcapi.CloseModel(modelID);
     });
 });
+
+
+
+/**
+ * Get all IFCSPACE from ifc file
+ * @param integer modelID 
+ * @returns array
+ */
+function getAllSpaces(modelID) {
+    // Get all the propertyset lines in the IFC file
+    let lines = ifcapi.GetLineIDsWithType(modelID, IFCSPACE);
+    let lineSize = lines.size();
+    let spaces = [];
+    for (let i = 0; i < lineSize; i++) {
+        // Getting the ElementID from Lines
+        let relatedID = lines.get(i);
+        // Getting Element Data using the relatedID
+        let relDefProps = ifcapi.GetLine(modelID, relatedID);
+        spaces.push(relDefProps);
+
+    }
+    return spaces;
+}
